118d43a3d66069e9a46a22fa54647466b3a3a7ce
Josh Reep <josh.reep@sstid.com>
2021-09-16 21:04:09 -0400

    adds color values constant

---
+++ theme/colorValues.ts
@@ -0,0 +1,12 @@
+const COLOR_VALUES = Object.freeze({
+    primary: '#00bcd4',
+    primaryDark: '#008ba3',
+    primaryLight: '#62efff',
+    secondary: '#8bc34a',
+    secondaryDark: '#5a9216',
+    secondaryLight: '#bef67a',
+    textColorDark: '#000',
+    textColorLight: '#fff',
+})
+
+export default COLOR_VALUES
