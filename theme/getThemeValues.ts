b71ef429b454ed43c047694a50312c474127e5a0
Josh Reep <josh.reep@sstid.com>
2021-09-16 21:04:26 -0400

    updates theme values with overrides

--- theme/getThemeValues.ts
+++ theme/getThemeValues.ts
@@ -1,4 +1,5 @@
 import { ColorSchemeName } from 'react-native'
+import COLOR_VALUES from './colorValues'
 
 export type Theme = ReturnType<typeof getThemeValues>
 
@@ -6,14 +7,11 @@ export default function getThemeValues(colorScheme: NonNullable<ColorSchemeName>
     const isDark = colorScheme === 'dark'
 
     return {
-        textColor: isDark ? '#fff' : '#000',
-        textColorLight: '#fff',
-        textColorDark: '#000',
-        primary: '#00bcd4',
-        primaryLight: '#62efff',
-        primaryDark: '#008ba3',
-        secondary: '#8bc34a',
-        secondaryLight: '#bef67a',
-        secondaryDark: '#5a9216',
+        ...COLOR_VALUES,
+        colorScheme,
+        isDark,
+        primaryText: isDark ? COLOR_VALUES.primaryLight : COLOR_VALUES.primaryDark,
+        secondaryText: isDark ? COLOR_VALUES.secondaryLight : COLOR_VALUES.secondaryDark,
+        textColor: isDark ? COLOR_VALUES.textColorLight : COLOR_VALUES.textColorDark,
     }
 }
