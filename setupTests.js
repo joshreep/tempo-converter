a6e4d8a6495e5ad2683b3d82840e9ff29d173ee3
Sam Borgognoni <62440937+sam-borg@users.noreply.github.com>
2023-02-13 20:52:34 -0500

    Automation Testing #5
    Included steps to begin using a testing framework "Jest".

---
+++ setupTests.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react";
+//How to set up automation testing #5
+//Using Jest - a popular testing framework.
+//Install by running this command: 'npm install --save-dev jest' in rood directory
+//Step 1: Write test cases to test various functionality of the tap tempo. (ex. testing if the tempo is 
+//calculated correctly when the user taps the button multiple times.)
+//Step 2: Run "inside of this file" the apporpriate global variables
+//Step 3: Run tests by running the following command 'npx jest'
+//Step 4: Debug failing tests: If any of the tests fail, you can use Jest's build in debuggin tools 
+
